ff6c92a65bf800b9fe397af60d1fbd125e0d9890
James Lindstorff <jlindstorff@gmail.com>
2011-03-05 13:17:43 +0100

    First stab at step 1 part a of the danish stemmer

--- src/ec_danish_stemmer.erl
+++ src/ec_danish_stemmer.erl
@@ -24,7 +24,8 @@
 %%--------------------------------------------------------------------
 stem(Word) ->
     LowerWord = string:to_lower(Word),
-    regions(LowerWord).
+    {ok, R1, _R2} = regions(LowerWord), %% R2 unused in danish stemmer
+    {ok, NewLowerWord, NewR1} = step1a(LowerWord, R1).
 
 %%====================================================================
 %% Internal functions
@@ -38,6 +39,8 @@ regions(Word) ->
     %% in R1, or is the null region at the end of the word if there
     %% is no such non-vowel.
     R1 = r1(Word),
+    %% TODO: R1 is adjusted so that the region before it contains 
+    %% at least 3 letters.
     R2 = r1(R1),
     {ok, R1, R2}.
 
@@ -63,8 +66,40 @@ r1([H | T], Last) ->
 
 is_vowel(Character) ->
     %% Classifies the character as being a vowel or not a vowel 
-    %% where not a vowel is a consonant in this context
     lists:member(Character, "aeiouyæøå").
+
+step1a(Word, R1) ->
+    %% Search for the longest among the following suffixes in R1, 
+    %% and perform the action indicated. Which is delete.
+    Step1suffix = ["erendes", "erende", "hedens", "endes", "erede", 
+		   "erens", "erets", "ernes", "ethed", "heden",
+		   "heder", "ered", "ende", "enes", "eren", "erne",
+		   "erer", "eres", "eret", "heds", "ene", "ens",
+		   "ere", "ers", "ets", "hed", "en", "er", "es",
+		   "et", "e"],
+    case step1asuffix(Step1suffix, R1) of
+	{ok, MatchedSuffix} ->
+	    LenMatchSuffix = string:len(MatchedSuffix),
+	    LenWord = string:len(Word),
+	    LenR1 = string:len(R1),
+	    NewWord = string:substr(Word, 1, LenWord - LenMatchSuffix),
+	    NewR1 = string:substr(R1, 1, LenR1 - LenMatchSuffix),
+	    {ok, NewWord, NewR1};
+	{nomatch} ->
+	    {ok, Word, R1}
+    end.
+
+step1asuffix([], _R1) ->
+    {nomatch};
+
+step1asuffix([H | T], R1) ->
+    Match = lists:suffix(H, R1),
+    if
+	Match == true -> 
+	    {ok, H};
+	true ->
+	    step1asuffix(T, R1)
+    end.
 
 %%====================================================================
 %% Testing
@@ -78,5 +113,16 @@ regions_test() ->
     ?assertMatch({ok, "imadversion", "adversion"}, regions("animadversion")),
     ?assertMatch({ok, "kled", []}, regions("sprinkled")),
     ?assertMatch({ok, "harist", "ist"}, regions("eucharist")).
+
+regions_bestemmelse_test() ->
+    ?assertMatch({ok, "temmelse", "melse"}, regions("bestemmelse")).
+
+step1a_bestemmelse_test() ->
+    Word = "bestemmelse",
+    {ok, R1, _R2} = regions(Word),
+    ?assertMatch({ok, "bestemmels", "temmels"}, step1a(Word, R1)).
+
+step1a_nomatch_test() ->
+    ?assertMatch({ok, "nomatch", "nomatch"}, step1a("nomatch", "nomatch")).
 
 -endif.
