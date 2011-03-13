1e57cff447f918abda874210f5247ff4fa063da2
James Lindstorff <jlindstorff@gmail.com>
2011-03-13 11:04:34 +0100

    Whitespace, excluding space, detection for the remove_punctuation function

--- src/ec_document_processing.erl
+++ src/ec_document_processing.erl
@@ -23,11 +23,14 @@
 %% Punctuation is defined as: !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 %%--------------------------------------------------------------------
 remove_punctuation(Document) when is_binary(Document) ->
-    << <<X>> || <<X>> <= Document, (not is_punctuation(<<X>>)) >>.
+    << <<X>> || <<X>> <= Document, include_character(<<X>>) >>.
     
 %%====================================================================
 %% Internal functions
 %%====================================================================
+include_character(X) ->
+    not (is_punctuation(X) orelse is_whitespace_control(X)).
+
 is_punctuation(X) ->
     %% Punctuation defined as  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
     ((<<16#21>> =< X andalso X =< <<16#2F>>) orelse
@@ -35,7 +38,9 @@ is_punctuation(X) ->
      (<<16#5B>> =< X andalso X =< <<16#60>>) orelse
      (<<16#7B>> =< X andalso X =< <<16#7E>>)).
 
-    
+is_whitespace_control(X) ->    
+    %% Whitespace control characters not including space
+    (<<16#09>> =< X andalso X =< <<16#0D>>).
 
 %%====================================================================
 %% Testing
@@ -47,8 +52,19 @@ remove_punctuation_test() ->
     Expected = <<"Remove the punctuation"/utf8>>,
     ?assertEqual(Expected, remove_punctuation(Doc)).
 
+include_character_test() ->
+    ?assert(include_character(<< "\x{C6}"/utf8 >>)),
+    ?assert(include_character(<< "\X{20}"/utf8 >>)),
+    ?assertNot(include_character(<< "\x{0D}"/utf8 >>)).
+
+
 is_punctuation_test() ->
     ?assert(is_punctuation(<<"!"/utf8>>)),
     ?assertNot(is_punctuation(<<"a"/utf8>>)).
+
+is_whitespace_control_test() ->
+    ?assert(is_whitespace_control(<<"\t"/utf8>>)),
+    ?assertNot(is_whitespace_control(<<" "/utf8>>)),
+    ?assertNot(is_whitespace_control(<<"g"/utf8>>)).
 
 -endif.
