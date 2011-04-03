fad3790377d3a334aede2b1d252871f4f8f49ccc
James Lindstorff <jlindstorff@gmail.com>
2011-04-03 19:00:58 +0200

    Started work on sport classifier demo

---
+++ src/ec_sport_demo.erl
@@ -0,0 +1,46 @@
+%%%-------------------------------------------------------------------
+%%% File    : ec_sport_demo.erl
+%%% Author  : James Lindstorff <james@ind-w510>
+%%% Description : Demo of classifier using the politiken.dk sport
+%%% documents in test/documents
+%%%
+%%% Created :  3 Apr 2011 by James Lindstorff <james@ind-w510>
+%%%-------------------------------------------------------------------
+-module(ec_sport_demo).
+
+%% API
+-export([train/0]).
+
+-define(DOCPATH, "test/documents").
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: 
+%% Description:
+%%--------------------------------------------------------------------
+train() ->
+    timer:start(),
+    {ok, Files} = file:list_dir(?DOCPATH),
+    lists:foreach(fun(File) ->
+			 [Prefix | Number] = string:tokens(File, "_"),
+			 Usable = class_start(Prefix),
+			 if 
+			     Usable =:= true ->
+				 Class = list_to_atom(Prefix),
+				 DocFile = string:join([?DOCPATH, File], "/"),
+				 io:fwrite("~p ~w ~p~n", [Prefix, Number, DocFile]),
+				 {ok, Doc} = file:read_file(DocFile),
+				 ec_trainer:train(Class, Doc),
+				 timer:sleep(500);
+			     true -> 0
+			 end
+		 end, Files).
+
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+class_start(P) ->
+    ("f1" =:= P) orelse ("fodbold" =:= P) orelse ("cykling" =:= P).
+    
